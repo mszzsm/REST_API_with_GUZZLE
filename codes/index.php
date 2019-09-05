7d2917b96577d52c86acc59c7d193b32d0050bdd
Michał Sztefanica <mikeshtefanitsa@gmail.com>
2019-09-05 17:44:27 +0200

    magic method GET

--- codes/index.php
+++ codes/index.php
@@ -2,8 +2,14 @@
     require '../vendor/autoload.php';
     use GuzzleHttp\Client;
 
-    $client = new Client();
-    $response = $client->request( 'GET',  'http://jsonplaceholder.typicode.com/posts/1');
-    var_dump($response);
-
+    $client = new Client(['base_uri' => 'http://jsonplaceholder.typicode.com/' ]);
+    
+    $response = $client->request( 'GET',  'posts/1');
     echo $response -> getBody();
+
+    // using magic method
+    $response1 = $client -> get('posts/1');
+    echo $response1->getBody();
+
+
+    
